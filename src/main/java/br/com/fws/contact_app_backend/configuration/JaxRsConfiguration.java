d87b11aeb5e923e82c2d02e8908a95c8cc90b82a
Fernando Furtado <feh.wilinando@gmail.com>
2016-11-15 18:01:36 -0200

    added JAX-RS listener configuration

---
+++ src/main/java/br/com/fws/contact_app_backend/configuration/JaxRsConfiguration.java
@@ -0,0 +1,10 @@
+package br.com.fws.contact_app_backend.configuration;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+
+@ApplicationPath("/v1")
+public class JaxRsConfiguration extends Application{
+}
